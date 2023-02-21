ca4cceb857306811c6671dd4222e734141afebc0
EasyZero <eminem54@naver.com>
2023-02-21 10:10:04 +0900

    feat: 홈 템플릿 수정 네비바, 리스트, 토큰 로직 추가

--- src/components/template/home/HomeTemplate.tsx
+++ src/components/template/home/HomeTemplate.tsx
@@ -1,31 +1,57 @@
-import React from 'react';
-import styles from './HomeTemplate.module.scss';
+'use client';
 
-const uri = 'https://accounts.google.com/o/oauth2/v2/auth?';
-const scope = 'scope=https://www.googleapis.com/auth/youtube&';
-const access = 'access_type=online&';
-const grant = 'include_granted_scopes=true&';
-const state = 'state=state_parameter_passthrough_value&';
-const res_type = 'response_type=token&';
-const redirect = 'redirect_uri=http://localhost:3000/login&';
-const client_id = 'client_id=38775711295-nq3or8dpd2u731jqf0027vqj4ngnoljp.apps.googleusercontent.com';
+import React, { useEffect } from 'react';
+import styles from './HomeTemplate.module.scss';
+import axios from 'axios';
+import NavBarOrganism from '@/components/organisms/NavBar/NavBarOrganism';
+import useAccessTokenRecoil from '@/hooks-recoil/useAccessTokenRecoil';
+import usePlayListRecoil from '@/hooks-recoil/usePlayListRecoil';
+import PlayListOrganism from '@/components/organisms/PlayList/PlayListOrganism';
+import VideoListOrganism from '@/components/organisms/VideoList/VideoListOrganism';
 
 const HomeTemplate = () => {
+  const { isEmptyToken, accessToken } = useAccessTokenRecoil();
+  const { playList, setPlayList, isPlayListsEmpty } = usePlayListRecoil();
+
+  useEffect(() => {
+    console.log(playList, 'home');
+    if (!isEmptyToken() && playList.length === 0) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/playlists', {
+          headers: {
+            Authorization: 'Bearer ' + accessToken,
+          },
+          params: {
+            mine: true,
+            maxResults: 50,
+            part: 'snippet',
+          },
+        })
+        .then((response) => {
+          // [ask]: response.data.item 안에 key값이 PlayListProps로 지정한 key값 보다 많아요. 괜찮은가요?
+          // 안 쓸것 같은 데이터는 빼고 set 해주는게 맞을까요?
+          setPlayList(response.data.items);
+          console.log(playList, 'home useEffect');
+        });
+    }
+  }, [isEmptyToken, accessToken, playList, setPlayList]);
+
   return (
-    <div>
-      <a href={`${uri + scope + access + grant + state + res_type + redirect + client_id}`} className={styles.link}>
-        로 그 인
-      </a>
+    <div className={styles.wrap}>
+      <NavBarOrganism />
+      {!isEmptyToken() && !isPlayListsEmpty() && <PlayListOrganism />}
+      <VideoListOrganism />
+      {/* <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Wxxqvg0QsAg"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe> */}
     </div>
   );
 };
 
 export default HomeTemplate;
-
-`
-http://localhost:3000/#state=state_parameter_passthrough_value
-&access_token=ya29.a0AVvZVspK23ktRuulKVL3P61TiubmfBI78OZVPGeVXkDm4LvEpvBQxOE0_Q0j78JNUXDGfUmmU8snJNJ1PFdLbPsltPCY0bLiw2r5NiHgTRIit9tYR5s0CS_D1TNQS1G9hmhUpAoWyAyeYErcBK-F6-iYYfMgaCgYKATASARMSFQGbdwaIyr6-sTv1RzTOVbBloxa6iA0163
-&token_type=Bearer
-&expires_in=3599
-&scope=https://www.googleapis.com/auth/youtube
-`;
